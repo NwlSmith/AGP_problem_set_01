68cab4563f79d8ac88f8f518420e260c7698655a
Nate Smith <me@nwlsmith.com>
2021-03-16 13:52:12 -0400

    Figured out first two. Got third, but wasn't satisfied, so I optimised it by caching char[] -> string[] in a dictionary.

--- Assets/Week7/SMITH_PS_W7.cs
+++ Assets/Week7/SMITH_PS_W7.cs
@@ -19,24 +19,151 @@ public class SMITH_PS_W7 : MonoBehaviour
      */
 
     // Return the reversed version of the input.
-    public string ReverseString(string toReverse)
-    {
-        if (toReverse.Equals(""))
-            return "";
-
-        return toReverse[toReverse.Length - 1] + ReverseString(toReverse.Substring(0, toReverse.Length - 2));
-    }
+    public string ReverseString(string toReverse) => (toReverse.Length == 1) ? toReverse : toReverse[toReverse.Length - 1] + ReverseString(toReverse.Substring(0, toReverse.Length - 1));
 
     // Return whether or not the string is a palindrome
-    public bool IsPalindrome(string toCheck)
+    public bool IsPalindrome(string toCheck) => toCheck.Length <= 1 ? true : toCheck[0].Equals(toCheck[toCheck.Length - 1]) && IsPalindrome(toCheck.Substring(1, toCheck.Length - 2));
+
+
+    private class CharCache
     {
-        return false;
+
+        Dictionary<string, string[]> charactersToStringCache = new Dictionary<string, string[]>();
+
+        private string CharToString(params char[] characters)
+        {
+            string key = "";
+            foreach (char c in characters)
+            {
+                key += c;
+            }
+            return key;
+        }
+
+        public bool Exists(params char[] characters)
+        {
+
+            string key = CharToString(characters);
+
+            Debug.Log($"Does cache contain {key}? {charactersToStringCache.ContainsKey(key)}");
+
+            return charactersToStringCache.ContainsKey(key);
+        }
+
+        public string[] Get(char[] characters)
+        {
+            string key = CharToString(characters);
+
+            string[] str = charactersToStringCache[key];
+
+            string toPrintS = "";
+
+            foreach (string s in str)
+            {
+                toPrintS += s + ", ";
+            }
+
+            Debug.Log($"Getting cache[{key}]. returning {toPrintS}");
+
+            return charactersToStringCache[key];
+        }
+
+        public void Set(char[] characters, string[] strings)
+        {
+            string key = CharToString(characters);
+
+            string toPrintS = "";
+
+            foreach (string s in strings)
+            {
+                toPrintS += s + ", ";
+            }
+
+            Debug.Log($"Setting cache[{key}] to {toPrintS}");
+
+
+            charactersToStringCache.Add(key, strings);
+        }
+
     }
+
+    private CharCache charCache = new CharCache();
 
     // Return all strings that can be made from the set characters using all characters.
     public string[] AllStringsFromCharacters(params char[] characters)
     {
-        return new string[0];
+        string toPrint = "";
+
+        foreach (char c in characters)
+        {
+            toPrint += c;
+        }
+        Debug.Log($"Called AllStringsFromCharacters(characters): {toPrint}");
+
+
+        if (characters.Length == 0)
+            return new string[0];
+
+        if (characters.Length == 1)
+        {
+            Debug.Log($"characters.Length == 1, returning {characters[0].ToString()}");
+            string[] ret = new string[1];
+            ret[0] = characters[0].ToString();
+            return ret;
+        }
+
+        if (charCache.Exists(characters))
+            return charCache.Get(characters);
+        
+
+        List<string> toReturnList = new List<string>();
+        
+        foreach (char c in characters)
+        {
+            // call allstringsfromchars on char array minus c, add c to every element from the returned allstringsfromchars
+
+            Debug.Log($"Looking at c = {c}");
+
+            char[] charsMinusC = new char[characters.Length - 1];
+
+            int curCharacterIndex = 0;
+            int curCharsMinusCIndex = 0;
+            while (curCharacterIndex < characters.Length)
+            {
+                if (characters[curCharacterIndex].Equals(c))
+                {
+                    curCharacterIndex++;
+                }
+                else
+                {
+                    charsMinusC[curCharsMinusCIndex] = characters[curCharacterIndex];
+                    curCharacterIndex++;
+                    curCharsMinusCIndex++;
+                }
+            }
+
+            toPrint = "";
+            foreach (char c2 in charsMinusC)
+            {
+                toPrint += c2;
+            }
+
+            Debug.Log($"charsMinusC = {toPrint}");
+
+            string[] allStrings = AllStringsFromCharacters(charsMinusC);
+
+            foreach (string str in allStrings)
+            {
+                toReturnList.Add(c + str);
+                Debug.Log($"added c + str = {c + str}");
+            }
+        }
+
+        // SUUUUUPER inefficient.
+
+        charCache.Set(characters, toReturnList.ToArray());
+
+        return toReturnList.ToArray();
     }
     
     // Return the sum of all the numbers given.
@@ -68,7 +195,20 @@ public class SMITH_PS_W7 : MonoBehaviour
         recursionTest.text =  "Recursion Problems\n<align=left>\n";
         recursionTest.text += Success(ReverseString("TEST") == "TSET") + " string reverser worked correctly.\n";
         recursionTest.text += Success(!IsPalindrome("TEST") && IsPalindrome("ASDFDSA") && IsPalindrome("ASDFFDSA")) + " palindrome test worked correctly.\n";
-        recursionTest.text += Success(AllStringsFromCharacters('A', 'B').Length == 2 && AllStringsFromCharacters('A').Length == 1 && AllStringsFromCharacters('A', 'B', 'C').Length == 6 && AllStringsFromCharacters('A', 'B', 'C', 'D', 'E', 'F', 'G').Length == 5040) + " all strings test worked correctly.\n";
+
+
+
+        Debug.Log($"NEW CALL!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        string[] strs = AllStringsFromCharacters('A', 'B', 'C', 'D', 'E');
+        string toPrint = "";
+        foreach(string str in strs)
+        {
+            toPrint += str + ", ";
+            Debug.Log(str);
+        }
+        Debug.Log($"In the end, all strings = {toPrint}");
+        charCache = new CharCache();
+        //recursionTest.text += Success(AllStringsFromCharacters('A', 'B').Length == 2 && AllStringsFromCharacters('A').Length == 1 && AllStringsFromCharacters('A', 'B', 'C').Length == 6 && AllStringsFromCharacters('A', 'B', 'C', 'D', 'E', 'F', 'G').Length == 5040) + " all strings test worked correctly.\n";
         recursionTest.text += Success(SumOfAllNumbers(1, 2, 3, 4, 5) == 15 && SumOfAllNumbers(1, 2, 3, 4, 5, 6, 7) == 28) + " sum test worked correctly.\n";
 
         sodaTest.text = "Soda Problem\n<align=left>\n";
